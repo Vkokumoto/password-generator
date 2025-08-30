8e2d4677ff8d899004481051e46beec78745002c
Victor Okumoto <okumotovictor@gmail.com>
2025-08-30 12:33:02 -0300

    teste

--- src/password_gen/generator.py
+++ src/password_gen/generator.py
@@ -5,7 +5,6 @@ def generate_password(length: int = 12) -> str:
     if length < 8:
         raise ValueError("A senha precisa ter pelo menos 8 caracteres.")
     
-    # lembrando: string.ascii_letters (todas as letras maiúsculas e minúsculas) e string.digits (zero a nove)
     chars = string.ascii_letters + string.digits + "!@#$%&*"
     password = "".join(random.choice(chars) for _ in range(length))
     return password
